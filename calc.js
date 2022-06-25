0338d5ca9084e4b3160f5dd68a1667a79f872820
Solubode001 <105518031+Solubode001@users.noreply.github.com>
2022-06-25 17:33:38 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+// display() function
+function display(val){
+    document.getElementById('result').value += val
+
+    return val
+}
+
+// solve() function
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+
+    return y
+}
+
+// clearScreen() 
+function clearScreen(){
+    document.getElementById('result').value = ''
+}
